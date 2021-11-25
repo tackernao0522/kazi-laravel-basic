c5884a80b3a7240d152348a43065d03ab9447507
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-11-25 17:39:58 +0900

    Eloquent-ORM-One-To-One-Relationships

--- server/app/Http/Controllers/CategoryController.php
+++ server/app/Http/Controllers/CategoryController.php
@@ -12,8 +12,8 @@ class CategoryController extends Controller
 {
     public function allCat()
     {
-        // $categories = Category::latest()->paginate(5);
-        $categories = DB::table('categories')->latest()->paginate(5);
+        $categories = Category::latest()->paginate(5);
+        // $categories = DB::table('categories')->latest()->paginate(5);
 
         return view('admin.category.index')->with('categories', $categories);
     }
