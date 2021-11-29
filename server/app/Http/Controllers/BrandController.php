5f78bcc6225a3a1ac1fa8133a6cacc84cb717d33
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-11-29 19:36:43 +0900

    Middleware Auth Users Access Control

--- server/app/Http/Controllers/BrandController.php
+++ server/app/Http/Controllers/BrandController.php
@@ -10,6 +10,11 @@ use Image;
 
 class BrandController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function allBrand()
     {
         $brands = Brand::latest()->paginate(5);
