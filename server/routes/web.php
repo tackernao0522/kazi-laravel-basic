de2c51b3e33595a2bfb7aad157ad0e97ca300b46
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-11-30 15:24:22 +0900

    Admin Panel Setup Part2

--- server/routes/web.php
+++ server/routes/web.php
@@ -47,6 +47,7 @@ Route::post('/multi/add', [BrandController::class, 'storeImg'])->name('store.ima
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     // $users = User::all();
-    $users = DB::table('users')->get();
-    return view('dashboard', compact('users'));
+    // $users = DB::table('users')->get();
+
+    return view('admin.index');
 })->name('dashboard');
