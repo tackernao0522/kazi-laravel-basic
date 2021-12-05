b1e9bbefdcff8c9c44c014528b744467a0e04fad
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-12-05 11:26:42 +0900

    Setup Home Page Portfolio Section

--- server/resources/views/admin/multipic/index.blade.php
+++ server/resources/views/admin/multipic/index.blade.php
@@ -1,10 +1,6 @@
-<x-app-layout>
-  <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      Multi Picture<b></b>
-    </h2>
-  </x-slot>
+@extends('admin.admin_master')
 
+@section('admin')
   <div class="py-12">
     <div class="container">
       <div class="row">
@@ -41,4 +37,4 @@
       </div>
     </div>
   </div>
-</x-app-layout>
+@endsection
