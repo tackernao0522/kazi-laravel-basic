5fac693a2187a61d2513a69b7c8e45c3d2381eb0
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-12-06 15:02:47 +0900

    Change Password Option Part3

--- server/app/Http/Controllers/ChangePass.php
+++ server/app/Http/Controllers/ChangePass.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ChangePass extends Controller
 {
@@ -11,8 +14,25 @@ class ChangePass extends Controller
         return view('admin.body.change_password');
     }
 
-    public function updatePassword()
+    public function updatePassword(Request $request)
     {
-        
+        $validateData = $request->validate([
+            'oldPassword' => 'required',
+            'password' => 'required|confirmed'
+        ]);
+
+        $hashedPassword = Auth::user()->password;
+        if (Hash::check($request->oldPassword, $hashedPassword)) {
+            $user = User::find(Auth::id());
+            $user->password = Hash::make($request->password);
+            $user->save();
+            Auth::logout();
+
+            return redirect()->route('login')
+                ->with('success', 'Password Is Change Successfully');
+        } else {
+            return redirect()->back()
+                ->with('error', 'Current Password Is Invalid');
+        }
     }
 }
